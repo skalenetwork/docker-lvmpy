2b096abb42d5b1c79076e9f89e608aa4123f7d63
badrogger <ivan@skalelabs.com>
2021-01-26 16:18:31 +0200

    Fix finalize.sh script

--- tests/finalize.sh
+++ tests/finalize.sh
@@ -10,7 +10,7 @@ vgremove schains
 echo "Cleaning up $BLOCK_DEVICE"
 pvremove $BLOCK_DEVICE
 echo "Unmount $BLOCK_DEVICE"
-umount -q $BLOCK_DEVICE
+umount $BLOCK_DEVICE || true
 
 BLOCK_DEVICE="$(losetup --list -a | grep loopbackfile.img |  awk '{print $1}')"
 if [ ! -z ${BLOCK_DEVICE} ]; then
