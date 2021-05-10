3789e3828603441d53aed9e0485180aff4365977
badrogger <ivan@skalelabs.com>
2021-05-10 13:26:13 +0300

    SKALE-4157 Modify tests

--- tests/healthcheck_test.py
+++ tests/healthcheck_test.py
@@ -1,31 +1,76 @@
+import docker
 import mock
-from docker.errors import APIError
+import pytest
 
-from healthcheck import healthcheck
-
-
-def test_healthcheck(vg):
-    result = healthcheck()
-    assert result == {'status': 'ok', 'data': 'Success'}
+from healthcheck import Healthcheck
 
 
-@mock.patch('healthcheck.create_volume_using_driver',
-            side_effect=APIError('Test error'))
-def test_healthcheck_creation_failed(create_volume_mock, vg):
-    result = healthcheck()
-    assert result == {
-        'status': 'error',
-        'data': 'Volume creation failed with: "Test error"'
-    }
+@pytest.fixture
+def hc():
+    h = Healthcheck(container='test-container', volume='test-volume')
+    yield h
+    h.remove_simple_container()
+    h.remove_volume_using_driver()
 
 
-@mock.patch('healthcheck.create_volume_using_driver')
-@mock.patch('healthcheck.remove_volume_using_driver',
-            side_effect=APIError('Test error'))
-def test_healthcheck_removing_failed(create_volume_mock,
-                                     remove_volume_using_driver, vg):
-    result = healthcheck(vg)
-    assert result == {
-        'status': 'error',
-        'data': 'Volume removing failed with: "Test error"'
-    }
+def test_healthcheck(vg, hc):
+    hc.run()
+
+
+def test_healthcheck_volume_creation_failed(vg, hc):
+    class VolumeCreationError(Exception):
+        pass
+
+    with mock.patch.object(hc, 'create_volume_using_driver',
+                           side_effect=VolumeCreationError('Test error')):
+        with pytest.raises(VolumeCreationError):
+            hc.run()
+
+
+def test_healthcheck_volume_removing_failed(vg, hc):
+    class VolumeRemovingError(Exception):
+        pass
+
+    with mock.patch.object(hc, 'remove_volume_using_driver',
+                           side_effect=VolumeRemovingError('Test error')):
+        with pytest.raises(VolumeRemovingError):
+            hc.run()
+
+
+def test_healthcheck_container_creation_failed(vg, hc):
+    class ContainerCreationError(Exception):
+        pass
+
+    with mock.patch.object(hc, 'create_simple_container',
+                           side_effect=ContainerCreationError('Test error')):
+        with pytest.raises(ContainerCreationError):
+            hc.run()
+
+
+def test_healthcheck_container_removing_failed(vg, hc):
+    class ContainerRemovingError(Exception):
+        pass
+
+    with mock.patch.object(hc, 'remove_simple_container',
+                           side_effect=ContainerRemovingError('Test error')):
+        # Docker won't be able to remove volume because it will be in use
+        with pytest.raises(docker.errors.APIError):
+            hc.run()
+
+
+def test_healthcheck_volume_status_failed(vg, hc):
+    class VolumeStatusError(Exception):
+        pass
+    with mock.patch.object(hc, 'check_volume_status',
+                           side_effect=VolumeStatusError('Test error')):
+        with pytest.raises(VolumeStatusError):
+            hc.run()
+
+
+def test_healthcheck_container_status_failed(vg, hc):
+    class ContainerStatusError(Exception):
+        pass
+    with mock.patch.object(hc, 'check_container_status',
+                           side_effect=ContainerStatusError('Test error')):
+        with pytest.raises(ContainerStatusError):
+            hc.run()
